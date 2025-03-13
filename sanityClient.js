e998fc3d64744757ebefa12a3ec006f93d46de92
ramsha1993 <rimshasaleem1993@gmail.com>
2025-03-13 16:01:05 +0500

    dashboard

--- sanityClient.js
+++ sanityClient.js
@@ -1,9 +1,9 @@
 import { createClient } from '@sanity/client';
 
 export const sanityClient = createClient({
-  projectId: process.env.NEXT_APP_SANITY_PROJECT_ID,  
+  projectId: '6kvdqvro',  
   dataset: 'production',
   useCdn: false,
   apiVersion: '2023-01-01',
-  token: process.env.NEXT_APP_SANITY_TOKEN, // Secure API token
+  token: 'skpTDyV2Onyh30A1Y40zRw0eUywaTovXTxUHIv8y9blJrVdtasSp4HnLQpoMTAlUsH8Hd1QueFeR7HffYzXRgnf7rdP6g5VL8zR5RhhqnftJrv6svPHNENfxS7zIflDbL7RksYl0TLkFejRtkFv6ECG0gOeMldU7Caq6jl8hl0KOZXl0t1U0', // Secure API token
 })
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -3,23 +3,25 @@ import { sanityClient } from '../../sanityClient'
 const index = () => {
 
   const [query, setquery] = useState([])
-  useEffect(()=>{
-    sanityClient.fetch('*[_type == "formSubmmission"]{name,email,message}')
-    .then(setquery)
-  },[]);
-  return (
-    <div>
-<h1>admin dashboard</h1>
-{
-  query.map((elem,index)=>{
-    <div key={index}>
-<p>{elem.name}</p>
-<p>{elem.email}</p>
-<p>{elem.message}</p>
-    </div>
-  })
-}
 
+  useEffect(() => {
+    sanityClient
+      .fetch('*[_type == "formSubmmission"]{name,email,message}')
+      .then(setquery)
+      .catch(console.error); // To catch errors
+  }, [])
+
+ return (
+    <div>
+      <h1>Admin Dashboard</h1>
+   { console.log(query)}
+      {query.map((elem, index) => (
+        <div key={index}>
+          <p>{elem.name}</p>
+          <p>{elem.email}</p>
+          <p>{elem.message}</p>
+        </div>
+      ))}
     </div>
   )
 }
